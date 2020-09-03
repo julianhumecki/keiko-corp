0ef0f4d5dccf2f16da957cc7307150f084a0bc69
julianhumecki <julian_humecki@yahoo.ca>
2020-09-02 20:26:03 -0400

    Optimized site by loading whatever is necessary, loading above the fold, removing unneccesary animations, deferring script loading, and by minimizing images

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
 $(document).ready(function(){
 
-	//Navigation menu scrollTo
-	$('header nav ul li a').click(function(event){
-		event.preventDefault();
-		var section = $(this).attr('href');
-		var section_pos = $(section).position();
-
-		if(section_pos){
-			$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
-		}
-		
-	});
-
-	$('.app_link').click(function(e){
-		event.preventDefault();
-		$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
-	});
-
-
-
-
+	
 
 
 
@@ -37,15 +18,6 @@ $(document).ready(function(){
 
 
 
-	//wow.js on scroll animations initialization
-	wow = new WOW(
-	    {
-		  animateClass: 'animated',
-		  mobile: false,
-		  offset: 50
-		}
-	);
-	wow.init();
 
 
 
@@ -54,27 +26,7 @@ $(document).ready(function(){
 
 
 
-	//parallax effect initialization
-	$('.hero').parallax("50%", 0.3);
 
-
-
-
-
-
-
-
-	//Nice scroll initialization
-	$("html").niceScroll({
-		scrollspeed: 50,
-		autohidemode : false,
-		cursorwidth : 8,
-		cursorborderradius: 8,
-		cursorborder : "0",
-		background : "rgba(48, 48, 48, .4)",
-		cursorcolor : '#1f1f1f',
-		zindex : 999
-	});
 
 
 
